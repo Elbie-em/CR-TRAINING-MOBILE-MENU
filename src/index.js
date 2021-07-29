3d57b8961e9ac8dce8812327fecbd42988de5f8e
Elbie-em <emoonga1993@gmail.com>
2021-07-29 15:26:08 +0200

    Fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-
-const menuBtn = document.getElementById('menu-btn')
+const menuBtn = document.getElementById('menu-btn');
 
 menuBtn.onclick = () => {
   document.getElementById('mobile-menu').style.display = 'unset';
-}
+};
 
-const closeMenuBtn = document.getElementById('close-menu-btn')
+const closeMenuBtn = document.getElementById('close-menu-btn');
 
 const closeMenu = () => {
   document.getElementById('mobile-menu').style.display = 'none';
-}
+};
 
-closeMenuBtn.onclick = closeMenu
-
+closeMenuBtn.onclick = closeMenu;
